01a7beae44a78dd207bf0abbdc5a61d89e5a72a9
GTNardy <gtnardy@gmail.com>
2021-06-01 09:43:07 -0300

    Update Index.lua

--- Client/Index.lua
+++ Client/Index.lua
@@ -9,8 +9,8 @@ Events:Subscribe("ExplodeFireworkSound", function(location)
     Sound(location, "TS_Fireworks::A_Firework_Explosion_Fizz", false, true, SoundType.SFX, 3, 1, 400, 100000)
 end)
 
--- Waits 3 second so the Sandbox can be loaded first
-Timer:SetTimeout(3000, function()
+-- Waits 100 ms so the Sandbox can be loaded first
+Timer:SetTimeout(100, function()
 	Package:Call("Sandbox", "AddSpawnMenuItem", {"TS_Fireworks", "tools", "FireworkGun", "Firework Gun", "assets///NanosWorld/SK_FlareGun.jpg"})
     return false
 end)
