78585b50b0bce3c6c7f74f6f18df048236749678
GTNardy <gtnardy@gmail.com>
2021-06-01 09:43:48 -0300

    Update Index.lua

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,16 +1,16 @@
 -- Subscribes to spawn and attach the Firework launch sound
 Events:Subscribe("SpawnFireworkSound", function(firework)
-    local sound = Sound(Vector(), "TS_Fireworks::A_Firework_Launch", false, true, SoundType.SFX, 1, 1.5, 400, 100000)
-    sound:AttachTo(firework)
+	local sound = Sound(Vector(), "TS_Fireworks::A_Firework_Launch", false, true, SoundType.SFX, 1, 1.5, 400, 100000)
+	sound:AttachTo(firework)
 end)
 
 -- Subscribes to spawn the Firework explosion sound
 Events:Subscribe("ExplodeFireworkSound", function(location)
-    Sound(location, "TS_Fireworks::A_Firework_Explosion_Fizz", false, true, SoundType.SFX, 3, 1, 400, 100000)
+	Sound(location, "TS_Fireworks::A_Firework_Explosion_Fizz", false, true, SoundType.SFX, 3, 1, 400, 100000)
 end)
 
 -- Waits 100 ms so the Sandbox can be loaded first
 Timer:SetTimeout(100, function()
 	Package:Call("Sandbox", "AddSpawnMenuItem", {"TS_Fireworks", "tools", "FireworkGun", "Firework Gun", "assets///NanosWorld/SK_FlareGun.jpg"})
-    return false
+	return false
 end)
