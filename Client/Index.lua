c01d9a0582f597f0de037d5777188328e475cd49
GTNardy <gtnardy@gmail.com>
2021-06-28 09:12:28 -0300

    Replace Timer with Package Load

--- Client/Index.lua
+++ Client/Index.lua
@@ -9,8 +9,7 @@ Events:Subscribe("ExplodeFireworkSound", function(location)
 	Sound(location, "TS_Fireworks::A_Firework_Explosion_Fizz", false, true, SoundType.SFX, 3, 1, 400, 100000)
 end)
 
--- Waits 100 ms so the Sandbox can be loaded first
-Timer:SetTimeout(100, function()
+Package:Subscribe("Load", function()
 	Package:Call("Sandbox", "AddSpawnMenuItem", {"TS_Fireworks", "tools", "FireworkGun", "Firework Gun", "assets///NanosWorld/Thumbnails/SK_FlareGun.jpg"})
 	return false
 end)
